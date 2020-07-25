1121a84e5dc3d14e70d07bf0119d59203428c11a
hauze-v <hauze_v@protonmail.com>
2020-07-25 19:44:23 -0400

    Added functionality for crossing off done todo items

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Setup DOM references to HTML elements
 const todoInputEl = document.querySelector(".todo__input")
 const todoListEl = document.querySelector(".todo__list")
-const todoItemEl = document.querySelectorAll(".todo__item")
+const todoItemEls = document.querySelectorAll(".todo__item")
 
 // Function to add input text to list
 function addListItem() {
@@ -16,6 +16,26 @@ function addListItem() {
       // Clear the todoInputEl field
       todoInputEl.value = "";
     }
+  }) 
+}
+
+// This doesn't work because of eventListener bubbling and propagation
+/* function toggleDone() {
+  // Loop through all the todoItems and add eventlistener to each
+  for (let todoItem of todoItemEls) {
+    todoItem.addEventListener("click", function() {
+      todoItem.classList.toggle("done")
+    })
+  }
+} */
+
+// Function that toggles css class "done" when todoItemEls are clicked (use parent element to avoid bubbling)
+function toggleDone() {
+  todoListEl.addEventListener("click", function(event) {
+    /// If the target click's nodeName is "LI" toggle the css done class
+    if (event.target.nodeName === "LI") {
+      event.target.classList.toggle("done")
+    }
   })
 }
 
@@ -28,4 +48,5 @@ function createListItem(text) {
   return newListElement;
 }
 
+toggleDone()
 addListItem()
